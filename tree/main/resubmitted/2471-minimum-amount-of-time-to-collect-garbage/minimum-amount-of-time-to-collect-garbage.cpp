deb231e8bd8b42da9b33cc5918a850d21c3e4ac1
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-02-01 16:03:24 +0530

    Time: 157 ms (80.27%) | Memory: 105.7 MB (20.29%) - LeetSync

---
+++ tree/main/resubmitted/2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    // step 1 : calculate pick up time and the last house index to pick garbage from for each type.
+    // step 2: using last house index of each type of garbage we calculte travel time.
+    // step 3: we finalize our ans using pick up time and travel time.
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        
+        int pickP=0;
+        int pickG=0;
+        int pickM=0;
+
+        int travelP=0;
+        int travelG=0;
+        int travelM=0;
+
+        int lastP=0;
+        int lastG=0;
+        int lastM=0;
+
+        //calculate pick time and travel time 
+        for(int i=0;i<garbage.size();i++){
+            string curr =garbage[i];
+
+            for(auto ch:curr){
+                if(ch=='P'){
+                    pickP++;
+                    lastP=i;;
+                }
+                else if(ch=='G'){
+                    pickG++;
+                    lastG=i;
+                }
+                else if(ch=='M'){
+                    pickM++;
+                    lastM=i;
+                }
+            }
+        }
+
+        //travel time
+        for(int i=0;i<lastP;i++){
+            travelP+=travel[i];
+        }
+        for(int i=0;i<lastG;i++){
+            travelG+=travel[i];
+        }
+        for(int i=0;i<lastM;i++){
+            travelM+=travel[i];
+        }
+
+        int finalAns = (pickP+travelP)+(pickG+travelG)+(pickM+travelM);
+        return finalAns;
+
+
+    }
+};
