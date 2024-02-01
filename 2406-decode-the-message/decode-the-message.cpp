a2409f3cdac6a199aa185a6cf97b05b328948993
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-02-01 13:51:04 +0530

    Time: 5 ms (43.55%) | Memory: 8.3 MB (18.25%) - LeetSync

--- 2406-decode-the-message/decode-the-message.cpp
+++ 2406-decode-the-message/decode-the-message.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     string decodeMessage(string key, string message) {
-        
-        unordered_map<char,char>cmap;
-        char ch = 'a';
-        for(auto i:key){
-            if(cmap.find(i)==cmap.end()){
-                if(i!=' '){
-                cmap[i]=ch;
-                ch++;
-                }
-                else{
-                    cmap[i]=i;
-                }
+        //creating own mapping
+        vector<char>cmap(256,0);
+        char start ='a';
+        for(auto ch:key){
+            if(ch!=' '&&cmap[ch]==0){
+                cmap[ch]=start;
+                start++;
             }
         }
-        cmap[' ']=' ';
-        // for(auto i:cmap){
-        //     cout<<i.first<<" "<<i.second<<endl;
-        // }
         string ans="";
-        for(auto i:message){
-            ans.push_back(cmap[i]);
+        for(auto ch :message){
+            if(ch !=' '){
+                ans.push_back(cmap[ch]);
+            }
+            else{
+                ans.push_back(' ');
+            }
         }
         return ans;
     }
