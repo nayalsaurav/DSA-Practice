0df13fb8fcff734c4be25dad7259bad1d9ea7f09
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-02-02 18:26:24 +0530

    Time: 14 ms (62.22%) | Memory: 22 MB (14.91%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
+    //morse voting algo
     int majorityElement(vector<int>& nums) {
-        int count = 0;
-        int candidate = 0;
-        
-        for (int num : nums) {
-            if (count == 0) {
+        int count =0;
+        int candidate=nums[0];//let majority element be 1st element
+
+        for(auto num:nums){
+            if(count==0){ // count ==0 means there is some other element occuring more.
                 candidate = num;
             }
-            
-            if (num == candidate) {
+            if(num==candidate){
                 count++;
-            } else {
+            }
+            else{
                 count--;
             }
         }
-        
         return candidate;
     }
 };
