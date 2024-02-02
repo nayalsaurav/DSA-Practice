bf513136f1547b73856a11cc770167c48f72fb27
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-02-02 23:39:42 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (6.43%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.cpp
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -3,7 +3,7 @@ public:
     string reverseOnlyLetters(string s) {
         int i = 0;
         int j = s.size() - 1;
-
+        // is alpha tells if a character is an alphabet or not
         while (i < j) {
             if (!isalpha(s[i])) {
                 i++;
