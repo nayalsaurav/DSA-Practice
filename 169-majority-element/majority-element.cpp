da36fbfe28d38dfce323f74fac7a13895a1b2aeb
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-02-02 18:27:41 +0530

    Update majority-element.cpp

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,6 +1,8 @@
+
 class Solution {
 public:
     //morse voting algo
+    //T.C = O(n)
     int majorityElement(vector<int>& nums) {
         int count =0;
         int candidate=nums[0];//let majority element be 1st element
@@ -17,5 +19,20 @@ public:
             }
         }
         return candidate;
+    }
+};
+
+// using sort 
+// T.C = O(nlogn)
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        // sort and mid method
+        sort(nums.begin(),nums.end());
+        int s = 0;
+        int e = nums.size()-1;
+        int mid = s + (e-s)/2;
+
+        return nums[mid];
     }
 };
