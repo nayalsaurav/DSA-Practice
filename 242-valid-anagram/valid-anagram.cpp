82422780ab921894195123334b16dd955a6a319c
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-02-03 19:50:05 +0530

    Time: 11 ms (42.62%) | Memory: 9.7 MB (5.09%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        if(s.size()!=t.size()){
+            return false;
+        }
+       unordered_map<char,int>smap;
+       unordered_map<char,int>tmap;
+       for(auto ch:s){
+           smap[ch]++;
+       }
+       for(auto ch:t){
+           tmap[ch]++;
+       }
+       return smap==tmap;
     }
 };
