2aaf4c30d0227403cff29141c8b0af76f5a4bb7e
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-02-03 19:40:17 +0530

    Time: 4 ms (86.02%) | Memory: 8.4 MB (10.59%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,22 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size()){
-            return false;
-        }
-       unordered_map<char,int>smap;
-       unordered_map<char,int>tmap;
-       for(auto ch:s){
-           smap[ch]++;
-       }
-       for(auto ch:t){
-           tmap[ch]++;
-       }
-       for(auto ch:s){
-           if(t.find(ch)==string::npos||smap[ch]!=tmap[ch]){
-               return false;
-           }
-       }
-       return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
