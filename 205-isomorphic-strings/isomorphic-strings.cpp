922fc7136785923a78263fc479931e05871903a0
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-02-03 15:45:14 +0530

    Time: 6 ms (64.04%) | Memory: 8.6 MB (21.98%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,26 +1,24 @@
+#include <vector>
+
 class Solution {
 public:
-    void createGenericMapping(string &str){
-        vector<char>cmap(256,0);
-        char start ='a';
-        for(auto &ch:str){
-            if(cmap[ch]==0){
-                cmap[ch]=start;
+    string createGenericMapping(string str) {
+        vector<char> cmap(256, 0);
+        char start = 'a';
+        string result;
+
+        for (auto ch : str) {
+            if (cmap[ch] == 0) {
+                cmap[ch] = start;
                 start++;
             }
-            ch = cmap[ch];
+            result.push_back(cmap[ch]);
         }
-    }
-    bool isIsomorphic(string s, string t) {
-        createGenericMapping(s);
-        createGenericMapping(t);
 
-        if(s==t){
-            return true;
-        }
-        else{
-            return false;
-        }
-        
+        return result;
+    }
+
+    bool isIsomorphic(string s, string t) {
+        return createGenericMapping(s) == createGenericMapping(t);
     }
 };
