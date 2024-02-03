f99ab1e4398f3bd7ced867acccafbd6fd4ef2cbc
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-02-03 15:27:25 +0530

    Time: 11 ms (27.01%) | Memory: 9.2 MB (14.12%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int i=0;
+        int j = s.size()-1;
+        string vowels = "aeiouAEIOU";
+        while(i<j){
+            if(vowels.find(s[i])==string::npos){
+                i++;
+            }
+            else if(vowels.find(s[j])==string::npos){
+                j--;
+            }
+            else{
+                swap(s[i],s[j]);
+                i++;
+                j--;
+            }
+        }
+        return s;
+    }
+};
