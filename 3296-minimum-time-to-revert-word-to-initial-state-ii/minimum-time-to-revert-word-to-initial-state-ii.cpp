2d2213163bcbee3d6e3716b0ff32a673f87e061d
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-02-04 22:15:23 +0530

    Time: 287 ms (65.38%) | Memory: 28.2 MB (38.46%) - LeetSync

--- 3296-minimum-time-to-revert-word-to-initial-state-ii/minimum-time-to-revert-word-to-initial-state-ii.cpp
+++ 3296-minimum-time-to-revert-word-to-initial-state-ii/minimum-time-to-revert-word-to-initial-state-ii.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-// T.C = O(n*n/k)
-    int minimumTimeToInitialState(string word, int k) {
+    int minimumTimeToInitialState(std::string word, int k) {
         int timeTaken = 0;
         int index = 0;
+        int wordSize = word.size();
+        int initialSize = wordSize;
         string initial = word;
 
-        while (index < word.size()) {
+        while (index < wordSize) {
             index += k;
             timeTaken++;
-            //The parameters of compare are (position1, length1, str, position2, length2)
-            if (index <= word.size() && initial.compare(0, word.size() - index, word, index, word.size() - index) == 0) { //t.c = O(n/k)
+
+            if (index <= wordSize &&equal(initial.begin(), initial.begin() + wordSize - index, initial.begin() + index)) {
                 return timeTaken;
             }
         }
