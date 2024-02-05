047940c5ef3c04a524ecddadb3501ecf5bf31c5a
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-02-05 22:21:48 +0530

    Time: 17 ms (62.67%) | Memory: 20.1 MB (35.05%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    string expand(int i,int j,string &s){
+    string expand(int i, int j, string& s) {
         int size = 0;
-        while(i>=0&&j<s.length()&&s[i]==s[j]){
-            size = j-i+1;
+        while (i >= 0 && j < s.length() && s[i] == s[j]) {
+            size = j - i + 1;
             i--;
             j++;
         }
-        string str = s.substr(i+1,size);
-        return str;
+        return s.substr(i + 1, size);
     }
-    string longestString(string &s1,string &s2){
-        if(s1.length()>s2.length()){
-            return s1;
-        }
-        else{
-            return s2;
-        }
+    string longestString(const string& s1, const string& s2) {
+        return s1.length() > s2.length() ? s1 : s2;
     }
+
     string longestPalindrome(string s) {
         string ans = "";
         int n = s.length();
-        for(int center=0;center<n;center++){
-            string odd = expand(center,center,s);
-            string even = expand(center,center+1,s);
-            // cout<<odd<<" "<<even<<endl;
-            string temp =longestString(even,odd);
-            ans = longestString(ans,temp);
+        for (int center = 0; center < n; center++) {
+            string odd = expand(center, center, s);
+            string even = expand(center, center + 1, s);
+            string temp = longestString(even, odd);
+            ans = longestString(ans, temp);
+            if ((n - center) * 2 + 1 <= ans.length() || (n - center) * 2 <= ans.length()) {
+            break;
+        }
         }
         return ans;
     }
