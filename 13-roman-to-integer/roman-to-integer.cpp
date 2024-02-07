61f5384f14955de1f704e92bc813e57924892e16
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-02-07 23:43:55 +0530

    Time: 15 ms (34.64%) | Memory: 12.7 MB (15.89%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -4,16 +4,22 @@ public:
         unordered_map<char, int> roman {
             {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
         };
-        int n = s.length();
-        reverse(s.begin(), s.end());
-        int num = roman[s[0]];
-        for (int i = 0; i < n - 1; i++) {
-            if (roman[s[i]] > roman[s[i + 1]]) {
-                num -= roman[s[i + 1]];
+        
+        int num = 0;
+        int prevValue = 0; // Keep track of the previous value to handle subtractive cases
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currentValue = roman[s[i]];
+            
+            if (currentValue < prevValue) {
+                num -= currentValue; // Subtract if current value is less than previous value
             } else {
-                num += roman[s[i + 1]];
+                num += currentValue; // Add if current value is greater than or equal to previous value
             }
+            
+            prevValue = currentValue; // Update previous value for the next iteration
         }
+        
         return num;
     }
 };
