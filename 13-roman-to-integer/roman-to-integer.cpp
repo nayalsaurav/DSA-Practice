de8975743da2747839e97d2dd961aa74ab608294
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-02-07 23:38:56 +0530

    Time: 27 ms (5.10%) | Memory: 12.6 MB (17.93%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> roman {
+            {'I',1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {' M', 1000 }
+        };
+        int n = s.length();
+        reverse(s.begin(),s.end());
+        int num =roman[s[0]];
+        for(int i=0;i+1<n;i++){
+            if(roman[s[i]]>roman[s[i+1]]){
+                num = num-roman[s[i+1]];
+            }
+            else{
+                num+=roman[s[i+1]];
+            }
+            cout<<num<<endl;
+        }
+        return num;
+       
+    }
+};
