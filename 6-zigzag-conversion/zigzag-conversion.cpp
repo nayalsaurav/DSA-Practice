a8c4536a5167a2f119467041ea239d8c41572b18
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-02-08 20:20:59 +0530

    Time: 3 ms (98.45%) | Memory: 12.9 MB (38.80%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        
         if (numRows == 1 || s.size() <= numRows)
-            return s;
-        vector<string>row(numRows);
+            return s; // If numRows is 1 or equal to the size of the string, no conversion needed
+        
+        vector<string> rows(numRows);
+        int currentRow = 0;
         bool goingDown = false;
-        int currentRow=0;
 
-        for(auto ch:s){
-            row[currentRow].push_back(ch);
-            if(currentRow==0||currentRow==(numRows-1)){
-                goingDown =!goingDown;
-            }
-            currentRow +=goingDown?1:-1;
+        for (char ch : s) {
+            rows[currentRow] += ch; // Append the character to the current row
+
+            if (currentRow == 0 || currentRow == numRows - 1)
+                goingDown = !goingDown; // Change direction when reaching the top or bottom row
+
+            currentRow += goingDown ? 1 : -1; // Move to the next or previous row
         }
 
-        string result="";
-        for(auto& i:row){
-            result+=i;
+        string result;
+        for (const string& row : rows) {
+            result += row; // Concatenate the rows to get the result
         }
-        return result; 
+        return result;
     }
 };
