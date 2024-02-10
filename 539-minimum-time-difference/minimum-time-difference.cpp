23be0a9990adfd29fa1da089798928d370aed4bb
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-02-10 16:22:24 +0530

    Time: 538 ms (5.00%) | Memory: 17.4 MB (10.85%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.cpp
+++ 539-minimum-time-difference/minimum-time-difference.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int findMinDifference(vector<string>& timePoints) {
-        vector<int> temp;
-
-        for (auto i : timePoints) {
-            int hh = stoi(i.substr(0, 2)) * 60;
-            int mm = stoi(i.substr(3, 2));
-            temp.push_back(hh + mm);
+        vector<int>time;
+        for(auto s:timePoints){
+            int hh = stoi(s.substr(0,2))*60;
+            int mm = stoi(s.substr(3,2));
+            time.push_back(mm+hh);
         }
-        sort(temp.begin(), temp.end());
-        for (auto i : temp) {
-            cout << i << " ";
+        sort(time.begin(),time.end());
+        int minTime = INT_MAX;
+        for(int i=0;i<time.size()-1;i++){
+            for(int j=i+1;j<time.size();j++){
+            int diff1 = abs(time[i]-time[j]);
+            int diff2 = abs(time[i]+1440-time[j]);
+            // cout<<diff1<<" "<<diff2<<endl;
+            minTime = min(minTime,min(diff1,diff2));
+            }
         }
-        cout << endl;
-        int ans = INT_MAX;
-        // circular check
-        for (int i = 0; i <temp.size(); i++) {
-            int currentDifference = temp[i]-temp[(i + 1) % temp.size()];
-            int circularDifference = temp[(i + 1) % temp.size()]-temp[i] + 1440;
-            int diff = min(abs(currentDifference), abs(circularDifference));
-            // cout<<diff<<endl;
-            ans = min(ans, diff);
-
-        }
-        return ans;
+        return minTime;
     }
 };
