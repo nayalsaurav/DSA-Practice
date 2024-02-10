893cff17a94aef3699a93d5acb84dd782750b231
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-02-10 15:23:46 +0530

    Time: 12 ms (92.54%) | Memory: 12 MB (45.44%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-        int n = s.length();
-        vector<int>count(n,0);
-        int i=0;
-        int j = 0;
-        while(j<n){
-            s[i]=s[j];
-            count[i]=1;
-            if(i>0&&s[i-1]==s[i]){
-                count[i]+=count[i-1];
+        int n = s.length(); 
+        vector<int> count(n, 0); // Vector to store counts of consecutive characters
+        int i = 0; // current position in the modified string
+        int j = 0; // iterate through the input string
+        
+        // Iterate through the input string
+        while (j < n) {
+            s[i] = s[j]; // Assign character at index j to position i in the string
+            count[i] = 1; // Initialize count for the current character
+            
+            // If the previous character is the same as the current one
+            if (i > 0 && s[i - 1] == s[i]) {
+                count[i] += count[i - 1]; // Increment count based on previous count
             }
-            if(count[i]==k){
-                i-=k;
+            
+            // If the count reaches k, remove k duplicates by moving i back
+            if (count[i] == k) {
+                i -= k;
             }
-            i++;j++;
+            
+            // Move to the next position in the string
+            i++;
+            j++;
         }
-        return s.substr(0,i);
+        
+        // Return a substring of the modified string from index 0 to i
+        return s.substr(0, i);
     }
 };
