552cd13a1c46170e6bc150ec61a7ffc23b6f785b
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-03-13 17:28:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (88.92%) - LeetSync

--- 415-add-strings/add-strings.cpp
+++ 415-add-strings/add-strings.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    string addStrings(string num1, string num2) {
-        string ans = "";
-        int carry = 0;
-        int i = num1.size() - 1;
-        int j = num2.size() - 1;
-
-        while (i >= 0 || j >= 0 || carry != 0) {
-            int sum = carry;
-            if (i >= 0) {
-                sum += num1[i--] - '0';
-            }
-            if (j >= 0) {
-                sum += num2[j--] - '0';
-            }
-            ans += to_string(sum % 10);
-            carry = sum / 10;
+    void solve(string &num1, string &num2,int &carry,int i,int j,string &ans){
+        if(i<0&&j<0&&carry==0){
+            return;
         }
-
-        reverse(ans.begin(), ans.end());
+        int sum = carry;
+        if(i>=0){
+            sum+=num1[i]-'0';
+            i--;
+        }
+        if(j>=0){
+            sum+=num2[j]-'0';
+            j--;
+        }
+        ans+='0'+(sum%10);
+        carry = sum/10;
+        solve(num1,num2,carry,i,j,ans);
+    }
+    string addStrings(string num1, string num2) {
+        string ans ="";
+        int i = num1.size()-1;
+        int j = num2.size()-1;
+        int carry =0;
+        solve(num1,num2,carry,i,j,ans);
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
