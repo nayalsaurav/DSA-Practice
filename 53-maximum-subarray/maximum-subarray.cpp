a32c5a104296520729be51f4ff5c9004a5763bb9
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-03-14 16:38:31 +0530

    Time: 87 ms (20.13%) | Memory: 70.3 MB (27.53%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -7,13 +7,23 @@ public:
         }
         int sum = 0;
         int maxi = INT_MIN;
+        int start =0;
+        int end = 0;
         for(int i=0;i<n;i++){
+            if(sum==0){
+                start = i;
+            }
             sum+=nums[i];
-            maxi = max(sum,maxi);
+            if(sum>maxi){
+                maxi = sum;
+                end = i;
+            }
+            
             if(sum<0){
                 sum=0;
             }
         }
+        cout<<"start : "<<start<<" "<<"end : "<<end<<endl;
         return maxi;
     }
 };
