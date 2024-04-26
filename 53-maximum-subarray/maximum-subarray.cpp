d25da587ac2239896e9d0dad2cc0c49b520283c2
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-04-26 20:05:39 +0530

    Time: 95 ms (5.02%) | Memory: 70.2 MB (29.26%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int n = nums.size();
-        if(n==1){
-            return nums[0];
-        }
-        int sum = 0;
-        int maxi = INT_MIN;
-        int start =0;
-        int end = 0;
-        for(int i=0;i<n;i++){
-            if(sum==0){
-                start = i;
-            }
-            sum+=nums[i];
-            if(sum>maxi){
-                maxi = sum;
-                end = i;
-            }
+    int solve(vector<int>& nums,int start,int end){
+        if(start>=end) return nums[start];
+        int maxleftbordersum=INT_MIN,maxrightbordersum = INT_MIN; // Corrected initial values
+        int mid = start+(end-start)/2;
+        int maxleftsum = solve(nums,start,mid);
+        int maxrightsum = solve(nums,mid+1,end);
+
+        //max cross boader sum
+        int leftbordersum=0;
+        int rightbordersum = 0;
+        for(int i=mid;i>=start;i--){
+            leftbordersum +=nums[i];
+            if(leftbordersum>maxleftbordersum) maxleftbordersum = leftbordersum;
             
-            if(sum<0){
-                sum=0;
-            }
         }
-        cout<<"start : "<<start<<" "<<"end : "<<end<<endl;
-        return maxi;
+        for(int i=mid+1;i<=end;i++){
+            rightbordersum +=nums[i];
+            if(rightbordersum>maxrightbordersum) maxrightbordersum = rightbordersum; // Corrected comparison
+        }
+        int crossbordersum = maxleftbordersum+maxrightbordersum;
+        return max(maxleftsum,max(maxrightsum,crossbordersum));
+
+
+
+    }
+    int maxSubArray(vector<int>& nums) {
+        return solve(nums,0,nums.size()-1);
     }
 };
