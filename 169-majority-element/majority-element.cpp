11bc32569bdcd2ff83535f6e868d4b2f0942fb73
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-06-19 13:12:15 +0530

    Time: 17 ms (59.40%) | Memory: 26.5 MB (99.23%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,38 +1,18 @@
-
 class Solution {
 public:
-    //morse voting algo
-    //T.C = O(n)
-    int majorityElement(vector<int>& nums) {
+    int majorityElement(vector<int>& arr) {
+        int ans = 0;
         int count =0;
-        int candidate=nums[0];//let majority element be 1st element
-
-        for(auto num:nums){
-            if(count==0){ // count ==0 means there is some other element occuring more.
-                candidate = num;
-            }
-            if(num==candidate){
-                count++;
-            }
-            else{
-                count--;
+        int elem = arr[0];
+        int n = arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[i]==elem) count++;
+            else if(arr[i]!=elem) count--;
+            if(count==0){
+                elem = arr[i];
+                count = 1;
             }
         }
-        return candidate;
-    }
-};
-
-// using sort 
-// T.C = O(nlogn)
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        // sort and mid method
-        sort(nums.begin(),nums.end());
-        int s = 0;
-        int e = nums.size()-1;
-        int mid = s + (e-s)/2;
-
-        return nums[mid];
+        return elem;
     }
 };
