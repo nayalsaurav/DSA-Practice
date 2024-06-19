e40a97d9071f17163465bfa77a70e154300b3734
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-06-19 12:03:12 +0530

    Time: 14 ms (51.23%) | Memory: 13.7 MB (45.65%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        map<int, vector<int>> mp;  // Use int for simplicity
+    vector<int> twoSum(vector<int>& arr, int target) {
+        map<int, int> mp;
         vector<int> ans;
-        int n = nums.size();
+        int n = arr.size();
         
-        // Fill the map with indices of each number
-        for(int i = 0; i < n; i++) {
-            mp[nums[i]].push_back(i);
-        }
-
-        // Traverse the array to find the required pair
-        for(int i = 0; i < n; i++) {
-            int rem = target - nums[i];
-            if(rem == nums[i] && mp[rem].size() > 1) {
-                ans.push_back(mp[rem][0]);
-                ans.push_back(mp[rem][1]);
-                return ans;
-            } else if(mp.find(rem) != mp.end() && mp[rem][0] != i) {
+        for(int i=0;i<n;i++){
+            int rem = target-arr[i];
+            if(mp.find(rem)!=mp.end()){
                 ans.push_back(i);
-                ans.push_back(mp[rem][0]);
+                ans.push_back(mp[rem]);
                 return ans;
             }
+               mp[arr[i]]=i;
         }
-        
         return ans;
+       
     }
 };
