fbf2721ca7b0a424192eacb65527592484f497f0
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-06-21 10:37:12 +0530

    Time: 14 ms (15.28%) | Memory: 16.8 MB (14.96%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,26 +3,26 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<pair<int,int>>loc;
+        vector<int>loc;
+        vector<int>row(n,0);
+        vector<int>col(m,0);
         for(int i=0;i<n;i++){
             for(int j =0;j<m;j++){
                 if(matrix[i][j]==0){
-                    loc.push_back({i,j});
+                    row[i]=1;
+                    col[j]=1;
                 }
             }
         }
-        for(auto i:loc){
-            int x = i.first;
-            int y = i.second;
-            int idx = 0;
-                while(idx<n){
-                matrix[idx++][y]=0;
+
+        for(int i=0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(row[i]||col[j]){
+                    matrix[i][j]=0;
                 }
-                int idy = 0;
-                while(idy<m){
-                matrix[x][idy++]=0;
-                }
+            }
         }
+        
 
     }
 };
