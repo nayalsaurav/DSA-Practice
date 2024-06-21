8546fc43f8782fa9a205e44a48adbff862363afe
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-06-21 12:11:10 +0530

    Time: 104 ms (33.39%) | Memory: 46.2 MB (42.43%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -3,21 +3,17 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
         int count = 0;
+        // hashmap
         int sum = 0;
 
-        // Hashmap to store the cumulative sum and its frequency
-        unordered_map<int, int> mp;
-        mp[0] = 1;  // To handle the case when the subarray starts from index 0
-
+        map<int, int> mp;
+        mp[0]=1;
         for (int i = 0; i < n; i++) {
             sum += nums[i];
-            
-            // Check if there is a subarray sum that equals the target
-            if (mp.find(sum - k) != mp.end()) {
-                count += mp[sum - k];
+            int rem = sum-k;
+            if (mp.find(rem) != mp.end()) {
+                count+=mp[rem];
             }
-            
-            // Increment the frequency of the cumulative sum
             mp[sum]++;
         }
 
