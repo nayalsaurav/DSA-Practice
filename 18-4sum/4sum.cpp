f0a134a7365fa0ff4995901c7865ed94f76af12e
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-06-22 22:03:42 +0530

    Time: 23 ms (80.65%) | Memory: 15.8 MB (79.29%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -2,32 +2,35 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
-        sort(nums.begin(),nums.end());
-        vector<vector<int>>ans;
-        set<vector<int>>st;
-        for(int i=0;i<n;i++){
-            // while(i!=0&&nums[i]==nums[i-1])i++;
-            for(int j=i+1;j<n;j++){
-                //two pointer
-                int k = j+1;
-                int l = n-1;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        // set<vector<int>>st;
+        for (int i = 0; i < n-3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            for (int j = i + 1; j < n-2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                // two pointer
+                int k = j + 1;
+                int l = n - 1;
+                while (k < l) {
+                    long long sum =
+                        1LL * (nums[i]) + nums[j] + nums[k] + nums[l];
 
-                while(k<l){
-                    long long sum = 1LL*(nums[i])+nums[j]+nums[k]+nums[l];
-
-                    if(sum==target){
-                        st.insert({nums[i],nums[j],nums[k],nums[l]});
+                    if (sum == target) {
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
                         k++;
                         l--;
-                        // while(k<l&&nums[k]==nums[k-1])k++;
-                        // while(k<l&&nums[l]==nums[l+1])l--;
-                    }
-                    else if(sum<target)k++;
-                    else l--;
+                        while (k < l && nums[k] == nums[k - 1]) k++;
+                        while (k < l && nums[l] == nums[l + 1]) l--;
+                    } else if (sum < target)
+                        k++;
+                    else
+                        l--;
                 }
             }
         }
-        for(auto v:st)ans.push_back(v);
+        // for(auto v:st)ans.push_back(v);
         return ans;
     }
 };
