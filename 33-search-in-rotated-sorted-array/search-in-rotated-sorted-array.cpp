f2ac542d38a302cd98e299b413224b39bde183e2
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-06-25 13:13:09 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (77.81%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,59 +1,34 @@
 class Solution {
 public:
-    int binarySearch(vector<int>&arr,int target,int lo,int hi){
-        while(lo<=hi){
-            int mid = lo+(hi-lo)/2;
-            if(arr[mid]==target){
-                return mid;
-            }
-            else if(arr[mid]<target){
-                lo=mid+1;
-            }
-            else{
-                hi=mid-1;
-            }
-        }
-        return -1;
-    }
-int check(vector<int>& arr,int mid){
-
-    if(arr[mid]<arr[0]){
-        return 1;
-    }
-    else{
-        return 0;
-    }
-}
     int search(vector<int>& nums, int target) {
         int n = nums.size();
         int lo = 0;
         int hi = n-1;
-        int idx = 0;
+
         while(lo<=hi){
-            int mid = lo+(hi-lo)/2;
-            if(check(nums,mid)==1){
-                idx = mid;
-                hi = mid-1;
+            int mid = (lo+hi)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>=nums[lo]){
+                //left is sorted
+                if(nums[lo]<=target&&target<nums[mid]){
+                    hi = mid-1;
+                }
+                else{
+                    lo = mid+1;
+                }
             }
             else{
-                lo = mid+1;
+                //right is sorted
+                if(nums[mid]<target&&target<=nums[hi]){
+                    lo = mid+1;
+                }
+                else{
+                    hi = mid-1;
+                }
             }
         }
-        
-        int i1 = binarySearch(nums,target,0,idx-1);
-        int i2 = binarySearch(nums,target,idx,n-1);
-
-        if(i1==-1&&i2==-1){
-            return -1;
-        }
-        else if(i1==-1){
-            return i2;
-        }
-        else{
-            return i1;
-        }
-
-
-
+        return -1;
     }
 };
